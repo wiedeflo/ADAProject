76d0f8f47f27e813103dde3975c1a8d07c1ca866
raphstrebel <raphael.strebel@epfl.ch>
2019-11-05 12:54:51 +0100

    changed path to match moved code

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -8,7 +8,7 @@ Contains constants such as file paths, columns names,...
 '''
 
 # Food inspection database
-FOOD_INSPECTIONS_PATH = 'data/chicago-food-inspections/food-inspections.csv'
+FOOD_INSPECTIONS_PATH = '../data/chicago-food-inspections/food-inspections.csv'
 FOOD_INSPECTIONS_COL_NAMES = ['inspection_id', 'DBA_name', 'AKA_name', 'license_num', 'facility_type', 'risk', 'address', 'city',
                               'state', 'zip', 'inspection_date', 'inspection_type', 'result', 'violations', 'lat', 'lng',
                               'location', 'historical_wards', 'zip_codes', 'community_areas', 'census_tracts', 'wards']
