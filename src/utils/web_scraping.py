9bca2fd0941ea4bfcc9cd173a163a6adccc4cee3
raphstrebel <raphael.strebel@epfl.ch>
2019-11-08 10:51:55 +0100

    web scraping to retrieve lng lat of address using google maps

--- src/utils/web_scraping.py
+++ src/utils/web_scraping.py
@@ -7,7 +7,7 @@ Python Version: 3.7.4
 Contains methods to retrieve the latitude and longitude of a Chicago city area
 '''
 
-import requests # to remove
+import requests
 import geopy
 from geopy.geocoders import Nominatim
 from utils import constants as cst
@@ -31,30 +31,31 @@ def get_lat_lng_from_area_name(area):
     return [lat, lng]
     
 
-def get_lat_lng_from_area_name_google_maps(area):
+def get_lat_lng_from_address_name_google_maps(address):
     """
     Retrieve the latitude and the longitude from a Chicago area.
     :param area: string
     :return: a list containing 2 floats (the latitude and the longitude)
     """
     
+    # Complete the address by adding "Chicago, United States"
+    complete_address = address + " " + cst.CHICAGO_ADDRESS
+    
     # get url to request
-    url = get_url_from_area_google_maps(area)
+    url = get_url_from_address_google_maps(complete_address)
     
     # get response from url
     r = requests.get(url)
     
     # parse response text to find string containing latitude and longitude
     lat_and_lng = r.text.partition(cst.LAT_LNG_HTML_POSITION_START)[2].partition(cst.LAT_LNG_HTML_POSITION_STOP)[0]
-       
-    print(lat_and_lng)
     
     # retrieve lat and lng
     lng, lat = lat_and_lng.split(',')[1:]
     
     return lat, lng
 
-def get_url_from_area_google_maps(area):
+def get_url_from_address_google_maps(address):
     """
     Retrieve the url corresponding to a 'google maps' query for a Chicago city area
     :param area: string
@@ -63,8 +64,8 @@ def get_url_from_area_google_maps(area):
     separator = '+'
     
     # get a string containing the words of the area joined by a '+', e.g. "Rogers Park" becomes "Rogers+Park"
-    area_formatted = separator.join(area.split(" "))
-    
+    address_formatted = separator.join(address.split(" "))
+        
     # return the url for the area
-    url = cst.CHICAGO_AREA_URL % (area_formatted)
+    url = cst.CHICAGO_AREA_URL % (address_formatted)
     return url
