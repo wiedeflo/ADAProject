220a583aa843a33c80867c9ddda4e65cf448ce3f
Garrido Ramas Jose <jogarrid@iccluster058.iccluster.epfl.ch>
2019-11-14 19:59:37 +0100

    Added some correlation work at the end of the notebook

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -21,7 +21,7 @@ AREA_NUM = 'community_area_num'
 FOOD_INSPECTIONS_PATH = DATA_PATH + 'food-inspections.csv'
 FOOD_INSPECTIONS_COL_NAMES = ['inspection_id', 'DBA_name', 'AKA_name', 'license_num', 'facility_type', 'risk', 'address', 'city',
                               'state', 'zip', 'inspection_date', 'inspection_type', 'result', 'violations', 'lat', 'lng',
-                              'location', 'historical_wards', 'zip_codes', 'community_areas', 'census_tracts', 'wards']
+                              'location']
 
 # Socio-economic indicators
 SOCIO_ECONOMIC_INDICATORS_PATH = DATA_PATH + 'Census_Data_-_Selected_socioeconomic_indicators_in_Chicago__2008___2012.csv'
@@ -30,6 +30,8 @@ SOCIO_ECONOMIC_COL_NAMES = ['community_area_num', 'community_area_name', 'housin
 # Life expectancy
 LIFE_EXPECTANCY_PATH = DATA_PATH + 'Public_Health_Statistics-_Life_Expectancy_By_Community_Area.csv'
 LIFE_EXPECTANCY_COL_NAMES = ['community_area_num', 'community_area_name', 'life_exp_1990', 'lower_95_perc_CI_1990', 'upper_95_perc_CI_1990', 'life_exp_2000', 'lower_95_perc_CI_2000', 'upper_95_perc_CI_2000', 'life_exp_2010', 'lower_95_perc_CI_2010', 'upper_95_perc_CI_2010']
+
+SOCIOECONOMIC_METRICS = ['housing_crowded_perc', 'housholds_below_poverty_perc', 'aged_16_or_more_unemployed_perc', 'aged_25_or_more_without_high_school_diploma_perc', 'per_capita_income', 'hardship_idx','life_exp_2010', 'aged_under_18_or_over_64_perc']
 
 # Chicago areas region bounds
 AREAS_PATH = DATA_PATH + 'Boundaries - Community Areas (current)/geo_export_3ffbc00d-9720-4c2b-b3c0-ed098b3b9ae7.shp'
